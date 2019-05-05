6cf0e6147d7e374bb7c0a8f7ae67d0e7cbbcf469
Chen <chen0gao@gmail.com>
2019-05-05 15:14:57 -0400

    Completed UI

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 function setup() {
-  createCanvas(1000, 650);
+  createCanvas(1100, 650);
   frameRate(30);
   background(250);
 
   var h = 50,
   w = 600;
   Bar = new Beam([h,w], [100,height/4]);
+  Bar.ui.x = Bar.x + Bar.w + 120;
+  Bar.ui.y = 100;
   Bar.color = color('rgb(255,255,255)');
   Bar.shearDiagram.display = 200;
   Bar.momentDiagram.display = (200 + 200);
-  Bar.creationDelay = 300; //ms
+  Bar.creationInterval = 300; //ms
 }
-
-
 
 function draw() {
 
--- src/Beam.js
+++ src/Beam.js
@@ -10,6 +10,8 @@
 * @property {color} color The color of the Beam. (default: white)
 * @property {number } creationInterval The inteval time of arrow creation in ms  (default 300)
 * @property {number } w The length of Beam in cm.
+* @property {number } ui.x The x position of arrow control UI system.
+* @property {number } ui.y The y position of arrow control UI system.
 
 * @property Beam.display() {method} Displays the Beam object.
 * @property Beam.update() {method} Updates the Beam object.
@@ -25,23 +27,35 @@ function Beam(size_, position_){
 
   this.createArrow = true;
 
+  this.ui = [];
+  this.ui.x = this.x + this.w + 100;
+  this.ui.y = 100;
+
   this.shearDiagram = new Array(this.w).fill(0);
   this.momentDiagram = new Array(this.w).fill(0);
   this.shearDiagram.display = false;
   this.momentDiagram.display = false;
 
   this.Arrow = [];
+  this.ui = [];
   this.lastCreateTime = new Date().getTime();
   this.creationInterval = 300; //ms
   this.color = color('rgb(255,255,255)');
 
+  // Arrow Force UI
+  this.input = createInput();
+  this.input.position(this.x+280, 22);
+  this.input.value(10);
+  this.input.elt.type = 'number';
+  this.input.size(100, AUTO)
+
   this.display = function() {
+    this.inputText = text( 'The magnitude of applied stress (kN):', this.x, 30);
 
     fill(this.color);
 
     // rect(x, y, w, h, [tl], [tr], [br], [bl])
     this.target = rect(this.x, this.y, this.w, this.h);
-
     createAxis('x',this,this.x,this.y + this.h)
 
     //create stands
@@ -51,41 +65,23 @@ function Beam(size_, position_){
     triangle(this.x-30, standY+30, this.x, standY, this.x+30, standY+30)
     triangle(this.x+this.w-30, standY+30, this.x+this.w, standY, this.x+this.w+30, standY+30)
 
-    //x axis
-    // line(x1, y1, x2, y2)
+    var diagramList = [this.shearDiagram, this.momentDiagram]
 
-    if (this.shearDiagram.display) {
-      var dist = this.shearDiagram.display
-      line(this.x, this.y+dist, this.x+this.w, this.y+dist)
-      renderLine(this.x, this.y+dist, this.shearDiagram, 'shear');
-      createAxis('x',this,this.x,this.y+dist)
-      createAxis('y',this,this.x,this.y + dist, 50)
+    for (let i = 0;i<diagramList.length;i++) {
+      let diagram = (i==0) ? 'shear' : 'moment'
+      let unit = (i==0) ? 'Stress (kN)' : 'Moment (kN*m)'
+
+      if (diagramList[i].display) {
+        var dist = diagramList[i].display
+        line(this.x, this.y+dist, this.x+this.w, this.y+dist)
+        renderLine(this.x, this.y+dist, diagramList[i], diagram);
+        createAxis('x',this,this.x,this.y+dist)
+        createAxis('y',this,this.x,this.y + dist, 50, unit)
+      }
     }
 
-    if (this.momentDiagram.display) {
-      var dist = this.momentDiagram.display
-      line(this.x, this.y+dist, this.x+this.w, this.y+dist)
-      renderLine(this.x, this.y+dist, this.momentDiagram, 'moment');
-      createAxis('x',this,this.x,this.y+dist)
-      createAxis('y',this,this.x,this.y + dist, 50)
-    }
-
-    // UI
-    var total = this.Arrow.filter(index => index.active).length
-    //Title
-    var posY = this.y - 80;
-    text( 'Name ', this.x + this.w + 100, posY);
-    text( 'Locat.', this.x + this.w + 140 + 20, posY);
-    text( 'Magnitude', this.x + this.w + 200 + 20, posY);
-
-    //Data
-    for (let j = 0;j<total;j++) {
-      var posY = this.y - 50 + (30*j);
-      textSize(16);
-      text( 'Force '+j, this.x + this.w + 100, posY);
-      text( (this.Arrow[j].target.x - this.x) / 100 + 'm', this.x + this.w + 150 + 20, posY);
-      text( '10 kN', this.x + this.w + 200 + 25, posY);
-    }
+    //UI Data
+    createUI(this)
   }
 
   this.update = function(){
@@ -160,6 +156,7 @@ function Beam(size_, position_){
     parent.Arrow[parent.Arrow.length-1].draggable = false;
     parent.Arrow[parent.Arrow.length-1].showComponents = false;
     parent.Arrow[parent.Arrow.length-1].active = false;
+    parent.Arrow[parent.Arrow.length-1].value = parent.input.value();
     parent.createArrow = false;
   }
 
@@ -175,7 +172,7 @@ function Beam(size_, position_){
       parent.Arrow[i].display();
 
       fill('rgb(214, 48, 49)');
-      text('10 kN', parent.Arrow[i].origin.x - 25, parent.forceArea.y - 5);
+      text(parent.Arrow[i].value + ' kN', parent.Arrow[i].origin.x - 25, parent.forceArea.y - 5);
     }
 
     //remove arrow when exit the forceArea
@@ -261,7 +258,7 @@ function Beam(size_, position_){
     var total = parent.Arrow.filter(index => index.active).length
     var sumForce = 0;
     for (let j = 0;j<total;j++) {
-      sumForce += 10*(parent.shearDiagram.length-(parent.Arrow[j].target.x-parent.x))
+      sumForce += parent.Arrow[j].value*(parent.shearDiagram.length-(parent.Arrow[j].target.x-parent.x))
     }
     // half = 10*total/2 //This only true when force is at L/2....
     half = sumForce/parent.shearDiagram.length
@@ -278,14 +275,14 @@ function Beam(size_, position_){
       //if (x>=a) = (x-a)^n
       for (let j = 0;j<total;j++) {
         if(i>=(parent.Arrow[j].target.x-parent.x)) {
-          parent.shearDiagram[i] -= 10
-          parent.momentDiagram[i] -= 10*(i-(parent.Arrow[j].target.x-parent.x) )/100
+          parent.shearDiagram[i] -= parent.Arrow[j].value
+          parent.momentDiagram[i] -= parent.Arrow[j].value*(i-(parent.Arrow[j].target.x-parent.x) )/100
         }
       }
     }
   }
 
-  function createAxis(type,parent,x,y,magnitude) {
+  function createAxis(type,parent,x,y,magnitude,unit) {
 
     switch(type) {
       case 'x':
@@ -295,6 +292,7 @@ function Beam(size_, position_){
         text(i, x+(i*100) - 3, y - 7);
         line(x+(i*100), y - 5, x+(i*100), y + 5) // axis line
       }
+      text('Distance (m)', x+(parent.w/100*100+20) - 3, y + 7); // Unit
       break;
 
       case 'y':
@@ -312,6 +310,64 @@ function Beam(size_, position_){
         line(x - 5, y -(i*10), x + 5, y -(i*10) ) // axis line
         line(x - 5, y +(i*10), x + 5, y +(i*10) ) // axis line
       }
+      push();
+      translate(x-50, y+40);
+      rotate(PI*3/2);
+      text(unit, 0, 0); // Unit
+      pop();
       break;
     }
+  }
+
+  function createUI(parent) {
+
+
+    var posY = parent.ui.y;
+    text( 'Name ', parent.ui.x, posY);
+    text( 'Locat.', parent.ui.x +40 + 20, posY);
+    text( 'Magni.', parent.ui.x +100 + 20, posY);
+    // text( 'Delete', parent.ui.x +160 + 20, posY);
+    // Arrow UI
+    var total = parent.Arrow.filter(index => index.active).length
+
+
+    for (let j = 0;j<total;j++) {
+
+      if(!parent.ui[j])
+      parent.ui[j] = []
+
+      var posY = parent.ui.y + 30 + (30*j);
+      textSize(16);
+      parent.ui[j]['text'] = text( 'Force '+j, parent.ui.x, posY);
+      parent.ui[j]['dist'] = text( (parent.Arrow[j].target.x - parent.x) / 100 + 'm', parent.ui.x + 50 + 20, posY);
+      parent.ui[j]['value'] = text( parent.Arrow[j].value + ' kN', parent.ui.x +100 + 25, posY);
+
+      if(!parent.ui[j]['delete']) {
+        parent.ui[j]['delete'] = createButton('Delete');
+        let button = parent.ui[j]['delete'].elt
+        let arrow = parent.Arrow[j]
+        parent.ui[j]['delete'].mousePressed(function(){deleteArrow(parent,arrow,button)});
+        parent.ui[j]['delete'].position(parent.ui.x +160 + 25, posY - 10);
+      }
+
+    }
+  }
+
+  function deleteArrow(parent,arrow,button) {
+    var total = parent.Arrow.filter(index => index.active).length
+    j = parent.Arrow.indexOf(arrow)
+
+    // Shift other delete btn up
+    for (let i=j;i<total;i++) {
+      var btn = parent.ui[i]['delete'];
+      btn.position(btn.position().x,  btn.position().y - 30);
+    }
+    parent.Arrow.splice(j, 1);
+    // parent.ui[j]['text'].remove();
+    // parent.ui[j]['dist'].remove();
+    // parent.ui[j]['value'].remove();
+    parent.ui.splice(j, 1);
+    // button.remove(); This is buggy
+    button.parentNode.removeChild(button);
+    singularityFn(parent)
   }
